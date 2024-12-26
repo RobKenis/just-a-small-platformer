38ceffec465e168185d7171adc89596a0107572f
Rob Kenis <rob.kenis@hotmail.com>
2024-12-26 13:47:14 +0100

    Make animated character
    - Make the character move using keyboard and controller
    - Left click to punch
    
    TODO:
    - Make attack mode better. This animation feels weird
    - Fix jump animation
    - Bind controller input for punch

---
+++ README.md
@@ -0,0 +1,6 @@
+# Just a small platformer
+
+## Assets
+
+- <https://kevins-moms-house.itch.io/four-seasons-platformer-tileset>
+- <https://free-game-assets.itch.io/free-tiny-hero-sprites-pixel-art>
---
+++ scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+var is_attacking = false
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	# Handle attacks
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+		animated_sprite.play("attack_punch")
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Get input direction: -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Set the animations
+	if !is_attacking:
+		if is_on_floor():
+			if direction == 0:
+				animated_sprite.play("idle")
+			else:
+				animated_sprite.play("walk")
+		else:
+			animated_sprite.play("jump")
+
+	# Move the character
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	is_attacking = false
